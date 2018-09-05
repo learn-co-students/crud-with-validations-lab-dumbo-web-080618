f606da05a576e77a6c7ebe2cf09468f3b5a5fa47
teasacura <teasacuraser@gmail.com>
2018-09-05 11:50:19 -0400

    add error messages

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,8 +13,9 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create(song_params)
+    @song = Song.new(song_params)
     if @song.valid?
+      @song.save
       redirect_to @song
     else
       render :new
