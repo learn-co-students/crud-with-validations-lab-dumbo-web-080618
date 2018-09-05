514344b8d1685c37f65d2723cf1406138fcc6cfb
Linh <linh44h@gmail.com>
2018-09-05 17:27:23 -0400

    done

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Song.create(title: "Happy", released: true, release_year: 2012, artist_name: "John Jay", genre: "Pop")
+Song.create(title: "Look At Me", released: false, release_year: 2002, artist_name: "Beyonce", genre: "Hiphop")
+Song.create(title: "Sunshine", released: true, release_year: 2018, artist_name: "Knock Lock", genre: "Ballad")
+Song.create(title: "Beautiful", released: false, release_year: 2011, artist_name: "Maria", genre: "Jazz")
